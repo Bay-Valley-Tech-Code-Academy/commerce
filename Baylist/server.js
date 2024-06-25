6c5d337b6b592c92c193716e9e914bffa4a2f578
Aveneil Lucena <neillucena97@gmail.com>
2024-06-25 00:25:34 -0700

    installed dotenv and mysql2 to fix process.env read issue for mysql database

--- Baylist/server.js
+++ Baylist/server.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv'; // Import dotenv for loading environment variables
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,21 +14,20 @@ const initializeDatabase = async () => {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: 'baylist-commerce-database',
+      database: process.env.DB_NAME,
     });
-    console.log('Connected to the MySQL database');
-    connection.end();
+    console.log('Connected to the MySQL database! You\'re using:', process.env.DB_NAME );
+    await connection.end(); // Close the connection after use
   } catch (error) {
     console.error('Error connecting to the database:', error);
   }
 };
 
+// Initialize database connection on application start
 initializeDatabase();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen
