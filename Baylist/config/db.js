17be421cf6947178619e097d176f3c6507c9307f
ToasterTecc <toastertechdotnet@gmail.com>
2024-07-16 14:16:34 -0700

    merging from fork and resolving conflicts

--- Baylist/config/db.js
+++ Baylist/config/db.js
@@ -5,17 +5,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-<<<<<<< HEAD
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-});
-
-export default pool;
-=======
 // Create a connection pool
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -30,4 +19,3 @@ const pool = mysql.createPool({
 
 // Export the pool for use in other modules
 export default pool.promise(); 
->>>>>>> c4e18a37faf26a35520b5ffc26e43dd2458f9e66
